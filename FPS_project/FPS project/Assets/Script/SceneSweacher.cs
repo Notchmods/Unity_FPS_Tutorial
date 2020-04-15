3f3939daac5ab4bf04378acb9c9b88b543febf19
Notchmods <Nomics3000@gmail.com>
2020-04-15 19:04:08 +1000

    My FPS tutorials files

---
+++ FPS_project/FPS project/Assets/Script/SceneSweacher.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSweacher : MonoBehaviour {
+    
+    public void FPS()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exitflow()
+    {
+        Application.Quit();
+        Debug.Log("You've exited the game");
+    }
+}
